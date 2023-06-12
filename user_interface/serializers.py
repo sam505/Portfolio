4456cef2373e1c1d0ba9e849dccf1865a63cdc28
sam505 <mainasamuel505@gmail.com>
2023-06-12 20:55:37 +0300

    Add api serializers

---
+++ user_interface/serializers.py
@@ -0,0 +1,45 @@
+from django.db.models.base import Model
+from rest_framework import serializers
+from .models import (User, InformationModel, EducationModel, SkillsModel, ExperienceModel, ProjectModel, MessageModel)
+
+
+class userSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class informationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InformationModel
+        fields = "__all__"
+
+
+class educationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EducationModel
+        fields = "__all__"
+
+
+class experienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExperienceModel
+        fields = "__all__"
+
+
+class projectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectModel
+        fields = "__all__"
+
+
+class skillsetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SkillsModel
+        fields = "__all__"
+
+
+class messageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MessageModel
+        fields = "__all__"
