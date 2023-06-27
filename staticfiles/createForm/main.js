1374157484810aa59e4a91acaf63c988a3d036bf
sam505 <mainasamuel505@gmail.com>
2023-06-28 00:27:49 +0300

    Add prod staticfiles

---
+++ staticfiles/createForm/main.js
@@ -0,0 +1,34 @@
+// Regular expression from W3C HTML5.2 input specification:
+// https://www.w3.org/TR/html/sec-forms.html#email-state-typeemail
+var emailRegExp = 
+/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+const Vue = window.vue;
+
+new Vue({
+  // root node
+  el: "#app",
+  // the instance state
+  methods: {
+    // validate by type and value
+    validate: function(type, value) {
+      if (type === "email") {
+        this.email.valid = this.isEmail(value) ? true : false;
+      }
+    },
+    // check for valid email adress
+    isEmail: function(value) {
+      return emailRegExp.test(value);
+    },
+    // check or uncheck all
+    checkAll: function(event) {
+      this.selection.features = event.target.checked ? this.features : [];
+    }
+  },
+  watch: {
+    // watching nested property
+    "email.value": function(value) {
+      this.validate("email", value);
+    }
+  }
+});
